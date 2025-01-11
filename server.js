c85ce8d8904d9d13eb8752da217bca67b5ebc76f
VenusYadav <venusyadav572@gmail.com>
2025-01-12 02:47:31 +0530

    Added react-scripts

--- server.js
+++ server.js
@@ -28,12 +28,13 @@ mongoose.connect(process.env.MONGODB_URL,{
 app.use('/api/chat', require('./routes/chatRoutes.js'));
 
 // Static Folder
-app.use(express.static(path.join(__dirname,'./client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-//Static Routes
+// Static Routes
 app.get('*', function (req, res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
+
 
 
 // start the server
